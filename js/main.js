de357d49b8f454abd1ac35d5aecb2060189e5758
Kevin Williams <kmwillia@gmail.com>
2016-03-06 23:57:01 -0500

    Simple always on image replacement added  - doesn't yet respect preferences  - needs more images, and proper srcset strings

--- js/main.js
+++ js/main.js
@@ -1,15 +1,65 @@
 'use strict';
 
 (function() {
+    var images = {
+        square: [
+            'resources/gyllenhaal/gyllenhaal-16.jpg',
+            'resources/gyllenhaal/gyllenhaal-15.jpg',
+            'resources/gyllenhaal/gyllenhaal-14.jpg',
+            'resources/gyllenhaal/gyllenhaal-13.jpg'
+        ],
+        portrait: [
+            'resources/gyllenhaal/gyllenhaal-12.jpg',
+            'resources/gyllenhaal/gyllenhaal-11.jpg',
+            'resources/gyllenhaal/gyllenhaal-10.jpg'
+        ],
+        landscape: [
+            'resources/gyllenhaal/gyllenhaal-9.jpg',
+            'resources/gyllenhaal/gyllenhaal-8.jpg',
+            'resources/gyllenhaal/gyllenhaal-7.jpg',
+            'resources/gyllenhaal/gyllenhaal-6.jpg',
+            'resources/gyllenhaal/gyllenhaal-5.jpg',
+            'resources/gyllenhaal/gyllenhaal-4.jpg',
+            'resources/gyllenhaal/gyllenhaal-3.jpg',
+            'resources/gyllenhaal/gyllenhaal-2.jpg',
+            'resources/gyllenhaal/gyllenhaal-1.jpg',
+        ]
+    };
+
     function traverse(el) {
-        var n,
-            walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
-        while(n = walk.nextNode()) gillefy(n);
+        var walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false),
+            n;
+        while(n = walker.nextNode()) gyllenText(n);
+        if(el.getElementsByTagName) {
+            Array.prototype.slice.call(el.getElementsByTagName('img')).forEach(gyllenImage);
+        }
     }
 
-    function gillefy(n) {
-        n.nodeValue = n.nodeValue.replace(/neanderthal/ig, 'Jake Gyllenhaal');
-        n.nodeValue = n.nodeValue.replace(/neandertal/ig, 'Jake Gyllenhall');
+    function gyllenText(n) {
+        window.requestAnimationFrame(function() {
+            n.nodeValue = n.nodeValue.replace(/neanderthal/ig, 'Jake Gyllenhaal');
+            n.nodeValue = n.nodeValue.replace(/neandertal/ig, 'Jake Gyllenhall');
+        });
+    }
+
+    function gyllenImage(img) {
+        var ratio = img.width / img.height,
+            src;
+        if(ratio === 1) {
+            src = chrome.extension.getURL(images.square[randomInRange(images.square.length)]);
+        } else if(ratio < 1) {
+            src = chrome.extension.getURL(images.portrait[randomInRange(images.portrait.length)]);
+        } else {
+            src = chrome.extension.getURL(images.landscape[randomInRange(images.landscape.length)]);
+        }
+        window.requestAnimationFrame(function() {
+            img.src = src || img.src;
+            img.srcset = src || img.srcset;
+        });
+    }
+
+    function randomInRange(n) {
+        return Math.floor(Math.random() * n);
     }
 
     function init() {
@@ -17,9 +67,7 @@
         var mutObs = new MutationObserver(function(mutations, observer) {
             mutations.forEach(function(mutation) {
                 if(mutation.type === 'childList') {
-                    Array.prototype.slice.call(mutation.addedNodes).forEach(function(n) {
-                        traverse(n);
-                    });
+                    Array.prototype.slice.call(mutation.addedNodes).forEach(traverse);
                 }
             });
         });
--- js/options.js
+++ js/options.js
@@ -1,15 +1,77 @@
-function saveOptions(options) {
-    var p = new Promise(function(resolve, reject) {
-        chrome.storage.sync.set(options, function() {
-            chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve();
-        });
-    });
-}
+'use strict';
 
-function getOptions(options) {
-    var p = new Promise(function(resolve, reject) {
-        chrome.storage.sync.get(options, function(items) {
-            chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve(items);
+(function() {
+    function saveOptions(options) {
+        return new Promise(function(resolve, reject) {
+            chrome.storage.sync.set(options, function() {
+                chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve();
+            });
         });
-    });
-}
+    }
+
+    function getOptions(options) {
+        return new Promise(function(resolve, reject) {
+            chrome.storage.sync.get(options, function(items) {
+                chrome.runtime.lastError ? reject(chrome.runtime.lastError) : resolve(items);
+            });
+        });
+    }
+
+    function init() {
+        getOptions({
+            imagesReplaceAll: false
+        }).then(function(options) {
+            updateView(options);
+        }).catch(function(error) {
+            console.log(error);
+        });
+
+        [].slice.call(document.getElementsByClassName('option-block')).forEach(function(el) {
+            el.addEventListener('click', optionsClickDispatch);
+        });
+    }
+
+    function updateView(options) {
+        console.log(options);
+        for(var key in options) {
+            if(options.hasOwnProperty(key)) {
+                let el = document.getElementById(key);
+                if(el) {
+                    let elType = el.getAttribute('type');
+                    if(elType === 'checkbox') {
+                        el.checked = options[key];
+                    } else {
+                        el.value = options[key];
+                    }
+                }
+            }
+        }
+    }
+
+    function updateData(options) {
+        console.log(options);
+        saveOptions(options).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    function optionsClickDispatch(e) {
+        if(e.target.classList.contains('option-input')) {
+            var options = {},
+                elType = e.target.getAttribute('type'),
+                value;
+
+            if(elType === 'checkbox') {
+                value = e.target.checked;
+            } else {
+                value = e.target.value;
+            }
+
+            options[e.target.id] = value;
+            updateData(options);
+        }
+    }
+
+    init();
+
+})();
